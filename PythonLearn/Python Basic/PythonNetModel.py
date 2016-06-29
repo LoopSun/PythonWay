47b558a4ff1a0055beaad292d09985569baceef4
Zhou Hua <zhouhua2297@yeah.net>
2016-06-29 18:19:56 +0800

    add some new files

--- PythonLearn/Python Basic/PythonNetModel.py
+++ PythonLearn/Python Basic/PythonNetModel.py
@@ -2,3 +2,19 @@
 #^.^ coding=utf-8 ^.^#
 from HelloWorld import cute_split_line
 
+from socket import socket
+from email.mime.text import MIMEText
+import smtplib
+
+msg = MIMEText("Hello, I am a human, no robot.", 'plain', 'utf-8')
+from_addr = "466105758@qq.com"
+password = "iqajsalyzthlbgij"
+
+to_addr = "zhouhua2297@yeah.com"
+smtp_server = "smtp.qq.com"
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
